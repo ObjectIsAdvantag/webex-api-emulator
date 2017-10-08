bfb62b43b5874a406fb4186bd71511b8496732bc
ObjectIsAdvantag <steve.sfartz@gmail.com>
2017-10-08 08:32:59 +0200

    added People details basic test

--- tests/people-me.js
+++ tests/people-me.js
@@ -4,19 +4,22 @@ const endpoint = process.env.ENDPOINT || "http://localhost:3210"
 const userToken = process.env.SPARK_TOKEN || "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const botToken = process.env.BOT_TOKEN || "ZYXWVUTSRQPONMLKJIHGFEDCBA9876543210"
 
-var suite = "GET people me: "
-describe("Chakram", function () {
-    it(suite + "no token", function () {
-        var response = chakram.get(endpoint + "/" + "people/me")
-        return expect(response).to.have.status(401);
+describe("GET people me", function () {
+    describe("no token", function () {
+        it("should respond 401", function () {
+            var response = chakram.get(endpoint + "/" + "people/me")
+            return expect(response).to.have.status(401);
+        })
     })
 
-    it(suite + "user ok", function () {
-        var response = chakram.get(endpoint + "/" + "people/me", {
-            "headers": {
-                "Authorization": "Bearer " + userToken
-            }
+    describe("user token", function () {
+        it("should show my details", function () {
+            var response = chakram.get(endpoint + "/" + "people/me", {
+                "headers": {
+                    "Authorization": "Bearer " + userToken
+                }
+            })
+            return expect(response).to.have.status(200)
         })
-        return expect(response).to.have.status(200)
     })
 })
--- tests/ping.js
+++ tests/ping.js
@@ -2,9 +2,11 @@ var chakram = require('chakram'), expect = chakram.expect;
 
 const endpoint = process.env.ENDPOINT || "http://localhost:3210";
 
-describe("Chakram", function() {
-    it("check API is alive", function () {
-        var response = chakram.get(endpoint + "/" );
-        return expect(response).to.have.status(200);
-    });
-});
+describe("Emulator Healthcheck", function () {
+    describe("ping", function () {
+        it("should respond 200", function () {
+            var response = chakram.get(endpoint + "/");
+            return expect(response).to.have.status(200);
+        })
+    })
+})
