eeef70b259fdb11603f509ae85d2c042406390e7
JP S <github@shipherd.com>
2018-03-19 14:07:02 -0400

    Capture bot membershipId in intercepted responses

--- bot-test/bot-test.js
+++ bot-test/bot-test.js
@@ -12,6 +12,13 @@ const sendSuccess = require('../utils').sendSuccess;
 let botTest = {};
 let testIdCounter = 1;
 
+// Load pre-registered tokens so we can get bot info
+var tokens = require('../tokens.json');
+if (!tokens) {
+    tokens = {};
+}
+
+
 botTest.middleware = function (req, res, next) {
 
     // Public resources
@@ -28,6 +35,17 @@ botTest.middleware = function (req, res, next) {
     }
 
     let botUnderTestEmail = req.app.locals.botUnderTestEmail
+    // The first time we see a request from the bot save its token
+    // so we can look up info about it later
+    if ((!req.app.locals.botActor) && (res.locals.person.emails[0] == botUnderTestEmail)) {
+        const auth = req.get("Authorization");
+        // Extract token -- no error checking since we already passed auth
+        const splitted = auth.match(/^Bearer\s([0-9a-zA-Z]*)$/);
+        // Retreive Person from token
+        const token = splitted[1];
+        req.app.locals.botActor = tokens[token];
+    }
+
     // Check if this is a test request that should generate bot requests 
     const botTestHeader = req.get("X-Bot-Responses");
     if (botTestHeader) {
